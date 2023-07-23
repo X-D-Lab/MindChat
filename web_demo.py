4186e39329a936a1323ac854fb55f69d1417241d
thomas-yanxin <yx20001210@163.com>
2023-07-23 12:07:09 +0800

    add web_demo

--- web_demo.py
+++ web_demo.py
@@ -1,61 +1,58 @@
-import gradio as gr
-import torch
-import torch.nn as nn
-from modelscope import Model, snapshot_download
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.generation.utils import GenerationConfig
+import time
 
-model_dir = snapshot_download("X-D-Lab/MindChat-Baichuan-13B",
+import gradio as gr
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from modelscope.hub.snapshot_download import snapshot_download
+
+title = "🐋MindChat: 漫谈心理大模型"
+
+description = """
+🔎 MindChat(漫谈): 旨在通过营造轻松、开放的交谈环境, 以放松身心、交流感受或分享经验的方式, 为用户提供隐私、温暖、安全、及时、方便的对话环境, 从而帮助用户克服各种困难和挑战, 实现自我成长和发展.
+
+🦊 无论是在工作场所还是在个人生活中, MindChat期望通过自身的努力和专业知识, 在严格保护用户隐私的前提下, 全时段全天候为用户提供全面的心理陪伴和倾听, 同时实现自我成长和发展, 以期为建设一个更加健康、包容和平等的社会贡献力量.
+
+🙅‍ 目前，MindChat还不能替代专业的心理医生和心理咨询师，无法做出专业的心理诊断报告。虽MindChat在训练过程中极致注重模型安全和价值观正向引导，但仍无法保证模型输出正确且无害，内容上模型作者及平台不承担相关责任。
+
+👏 欢迎关注：[MindChat Github](https://github.com/X-D-Lab/MindChat)
+"""
+
+submit_btn = '发送'
+
+retry_btn = '🔄 重新生成'
+
+undo_btn = '↩️ 撤销'
+
+clear_btn = '🗑️ 清除历史'
+
+cache_dir = './'
+
+
+snapshot_download('X-D-Lab/MindChat-7B',
+                              cache_dir=cache_dir,
                               revision='v1.0.0')
 
-tokenizer = AutoTokenizer.from_pretrained(model_dir,
-                                          use_fast=False,
-                                          trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(model_dir,
-                                             device_map="auto",
-                                             torch_dtype=torch.float16,
-                                             trust_remote_code=True)
-model.generation_config = GenerationConfig.from_pretrained(model_dir)
+tokenizer = AutoTokenizer.from_pretrained(cache_dir + "X-D-Lab/MindChat-7B", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(cache_dir + "X-D-Lab/MindChat-7B", trust_remote_code=True).cuda()
+model = model.eval()
 
 
-def clear_session():
-    return []
-
-
-def predict(input, history):
+def stream_predict(message, history):
+    dictionary  = {
+        'prompt': message
+    }
+    print(dictionary)
     if history is None:
         history = []
-    model_input = []
-    for chat in history:
-        model_input.append({"role": "user", "content": chat[0]})
-        model_input.append({"role": "assistant", "content": chat[1]})
-    model_input.append({"role": "user", "content": input})
-    print(model_input)
-    response = model.chat(tokenizer, model_input)
-    history.append((input, response))
-    history = history[-20:]
-    return '', history
+    
+    response, history = model.chat(tokenizer, message, history=history)
+    print(history)
 
+    for i in range(len(response)):
+        time.sleep(0.03)
+        yield  response[: i+1]
 
-block = gr.Blocks()
+demo = gr.ChatInterface(stream_predict, title=title, description=description, cache_examples=True, submit_btn=submit_btn, retry_btn=retry_btn, clear_btn=clear_btn, undo_btn=undo_btn).queue()
 
-with block as demo:
-    gr.Markdown("""<h1><center>MindChat-13B</center></h1>
-     <center>MindChat-13B：漫谈心理大模型，期望通过自身的努力和专业知识, 在严格保护用户隐私的前提下, 全时段全天候为用户提供全面的心理支持和诊疗帮助, 同时实现自我成长和发展, 以期为建设一个更加健康、包容和平等的社会贡献力量.</center>
-    """)
-    chatbot = gr.Chatbot(label='MindChat-13B')
-    message = gr.Textbox()
-    message.submit(predict,
-                   inputs=[message, chatbot],
-                   outputs=[message, chatbot])
-    with gr.Row():
-        clear_history = gr.Button("🧹 清除历史对话")
-        send = gr.Button("🚀 发送")
-
-    send.click(predict, inputs=[message, chatbot], outputs=[message, chatbot])
-    clear_history.click(fn=clear_session,
-                        inputs=[],
-                        outputs=[chatbot],
-                        queue=False)
-
-demo.queue().launch(height=800, share=False)
+if __name__ == "__main__":
+    demo.launch()
